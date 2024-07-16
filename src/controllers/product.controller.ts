9b3c05e4c9eae550b981f2a18c9fcb7f1400f3e8
Artsm14 <antonioartur123456@gmail.com.com>
2024-07-16 18:11:34 -0300

    atualizando controllers

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,17 +1,62 @@
 import { Request, Response} from 'express'
 import AppDataSource from '../connection'
 import { Product } from '../entities/product.entity'
+import { Repository } from 'typeorm'
+import { error } from 'console'
+
 
 class ProductController {
+  private productRepository : Repository<Product>
+  constructor(){
+    this.productRepository = AppDataSource.getRepository(Product)
+  }
+
   async findAll(request: Request, response: Response): Promise<Response> {
     const productRepository = AppDataSource.getRepository(Product)
 
     const products = await productRepository.find()
 
 
+
     return response.status(200).send({
       data: products
     })
+  }
+
+  async create(request: Request, response: Response): Promise<Response> {
+    const { name , description, weight} = request.body
+
+    const product = new Product
+    product.name = name
+    product.weight= weight
+    product.description = description
+
+    const productRepository = AppDataSource.getRepository(Product)
+
+    const productDb = await productRepository.save(product)
+
+    return response.status(201).send({
+      data: productDb
+    })
+  }
+
+  async findOne(request: Request, response: Response): Promise<Response> {
+    const id: string = request.params.id
+
+
+    const productRepository = AppDataSource.getRepository(Product)
+    const product = await productRepository.findOneBy({ id })
+
+    if (!product) {
+      return response.status(404).send({
+        error: 'Product not found'
+      })
+    }
+
+    return response.status(200).send({
+      data: product
+    })
+
   }
 }
 
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import bodyParser from "body-parser"
 
 dotenv.config()
 
@@ -9,7 +10,14 @@ import productController from './controllers/product.controller'
 const PORT = process.env.PORT || 8080
 
 const app = express()
+//middleware
+app.use(bodyParser.json())
+//routes
 app.get('/api/products', productController.findAll)
+app.post('/api/products', productController.create)
+app.get('/api/products/:id', productController.findOne)
+
+
 app.get('/', (request, response) => {
   response.send("Server up")
 })
