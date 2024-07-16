3295e6bd7c056284c8da6cc91a67adc43c3d94b6
Artsm14 <antonioartur123456@gmail.com.com>
2024-07-16 11:48:09 -0300

    chore: configs

---
+++ src/connection.ts
@@ -0,0 +1,26 @@
+import "reflect-metadata"
+import { DataSource } from "typeorm";
+
+const AppDataSource = new DataSource({
+  type: "postgres",
+  host: "localhost",
+  port: 5432,
+  username: "admin",
+  password: "admin",
+  database: "ts_node",
+  synchronize: true,
+  logging: true,
+  entities: [
+    __dirname + '/**/*.entity.ts'
+  ],
+  subscribers: [],
+  migrations: [],
+})
+
+AppDataSource.initialize()
+              .then(() => console.log('connected sucess'))
+              .catch((error) => console.log(error))
+
+
+export default AppDataSource
+
